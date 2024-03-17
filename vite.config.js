97fe6c2f981c331c4d818dc0165c2b0f7f45beab
Finley Siebert <finley@siebertmedia.nl>
2024-03-17 13:17:17 +0100

    Update for vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/create-ticket.js',
+                'resources/css/create-ticket.css'
             ],
             refresh: true,
         }),
