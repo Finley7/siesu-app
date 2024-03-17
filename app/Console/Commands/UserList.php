6a1abfc16b001db0706da6bbb35d4eaee75fb373
Finley Siebert <finley@siebertmedia.nl>
2024-03-17 13:50:48 +0100

    Added userlist command

---
+++ app/Console/Commands/UserList.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class UserList extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:user-list';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::all()->select('id', 'name', 'email', 'role');
+        $this->table(['Id', 'Name', 'Email', 'Role'], $users);
+    }
+}
